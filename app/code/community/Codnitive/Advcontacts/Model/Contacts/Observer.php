64020c6f9f1444177e66c2b2ede76cde658a6480
Omid <hbarza@gmail.com>
2018-11-07 11:21:40 +0330

    init

---
+++ app/code/community/Codnitive/Advcontacts/Model/Contacts/Observer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * CODNITIVE
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE_EULA.html.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.codnitive.com/en/terms-of-service-softwares/
+ * http://www.codnitive.com/fa/terms-of-service-softwares/
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade to newer
+ * versions in the future.
+ *
+ * @category   Codnitive
+ * @package    Codnitive_Advcontacts
+ * @author     Hassan Barza <support@codnitive.com>
+ * @copyright  Copyright (c) 2012 CODNITIVE Co. (http://www.codnitive.com)
+ * @license    http://www.codnitive.com/en/terms-of-service-softwares/ End User License Agreement (EULA 1.0)
+ */
+
+class Codnitive_Advcontacts_Model_Contacts_Observer extends Mage_Core_Model_Abstract
+{
+    public function checkContactsCaptcha($observer)
+    {
+        $formId = 'contacts';
+        $captchaModel = Mage::helper('captcha')->getCaptcha($formId);
+        if ($captchaModel->isRequired()) {
+            $controller = $observer->getControllerAction();
+            $word = $this->_getCaptchaString($controller->getRequest(), $formId);
+            if (!$captchaModel->isCorrect($word)) {
+                Mage::getSingleton('customer/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
+                $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
+                $url =  Mage::getUrl('contacts');
+                $controller->getResponse()->setRedirect($url);
+            }
+        }
+        return $this;
+    }
+    
+    protected function _getCaptchaString($request, $formId)
+    {
+        $captchaParams = $request->getPost(Mage_Captcha_Helper_Data::INPUT_NAME_FIELD_VALUE);
+        return $captchaParams[$formId];
+    }
+}
